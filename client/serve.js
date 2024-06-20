d6e6835da0808219f3ae11e4a601a6a71c9f4d02
Backend Dev <backend@dev.com>
2024-06-20 20:24:09 +0530

    Front

--- client/serve.js
+++ client/serve.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const path = require('path');
+// Example using import syntax in ES module
+import express from 'express';
+import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Serve static files from the build folder
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // Handle GET requests to serve the index.html file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // Start the server
